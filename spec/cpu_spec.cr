4be634b02aac96b93f3edfb4a4b4959a22b8d548
Julien Reichardt <git@jrei.ch>
2019-11-15 16:09:18 +0100

    Use assert_cpu_load

--- spec/cpu_spec.cr
+++ spec/cpu_spec.cr
@@ -1,13 +1,23 @@
 require "spec"
 require "../src/cpu"
 
+def assert_cpu_load(&)
+  # Wait enough time to have measurable load
+  cpu_usage = 0
+  9.times do
+    sleep 1
+    cpu_usage = yield
+
+    break if 0 < cpu_usage <= 100
+  end
+  cpu_usage.should be > 0
+  cpu_usage.should be <= 100
+end
+
 describe Hardware::CPU do
   it "returns the usage compared to a previous CPU'" do
     cpu = Hardware::CPU.new
-    sleep 6
-    cpu_usage = cpu.usage Hardware::CPU.new
-    cpu_usage.should be > 0
-    cpu_usage.should be <= 100
+    assert_cpu_load { cpu.usage Hardware::CPU.new }
   end
 
   it "parses the last field (guest_nice)" do
@@ -16,9 +26,6 @@ describe Hardware::CPU do
 
   it "returns the usage by mutating self" do
     cpu = Hardware::CPU.new
-    sleep 6
-    cpu_usage = cpu.usage!
-    cpu_usage.should be > 0
-    cpu_usage.should be <= 100
+    assert_cpu_load { cpu.usage! }
   end
 end
