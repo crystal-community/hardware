afdde49a4c46b025ee5ecd71b94391acf4e12cc9
Gabriel Aires Guedes <airesgabriel@gmail.com>
2021-11-26 11:29:02 -0300

    bugfixes
    Use KEYS constant to initialize @data, fix definition of getters

--- src/pid/status.cr
+++ src/pid/status.cr
@@ -1,74 +1,73 @@
 struct Hardware::PID::Status
-  getter data : Hash(String, String) = {
-    "Name"                       => "",
-    "Umask"                      => "",
-    "State"                      => "",
-    "Tgid"                       => "",
-    "Ngid"                       => "",
-    "Pid"                        => "",
-    "PPid"                       => "",
-    "TracerPid"                  => "",
-    "Uid"                        => "",
-    "Gid"                        => "",
-    "FDSize"                     => "",
-    "Groups"                     => "",
-    "NStgid"                     => "",
-    "NSpid"                      => "",
-    "NSpgid"                     => "",
-    "NSsid"                      => "",
-    "VmPeak"                     => "",
-    "VmSize"                     => "",
-    "VmLck"                      => "",
-    "VmPin"                      => "",
-    "VmHWM"                      => "",
-    "VmRSS"                      => "",
-    "RssAnon"                    => "",
-    "RssFile"                    => "",
-    "RssShmem"                   => "",
-    "VmData"                     => "",
-    "VmStk"                      => "",
-    "VmExe"                      => "",
-    "VmLib"                      => "",
-    "VmPTE"                      => "",
-    "VmSwap"                     => "",
-    "HugetlbPages"               => "",
-    "CoreDumping"                => "",
-    "THP_enabled"                => "",
-    "Threads"                    => "",
-    "SigQ"                       => "",
-    "SigPnd"                     => "",
-    "ShdPnd"                     => "",
-    "SigBlk"                     => "",
-    "SigIgn"                     => "",
-    "SigCgt"                     => "",
-    "CapInh"                     => "",
-    "CapPrm"                     => "",
-    "CapEff"                     => "",
-    "CapBnd"                     => "",
-    "CapAmb"                     => "",
-    "NoNewPrivs"                 => "",
-    "Seccomp"                    => "",
-    "Seccomp_filters"            => "",
-    "Speculation_Store_Bypass"   => "",
-    "SpeculationIndirectBranch"  => "",
-    "Cpus_allowed"               => "",
-    "Cpus_allowed_list"          => "",
-    "Mems_allowed"               => "",
-    "Mems_allowed_list"          => "",
-    "voluntary_ctxt_switches"    => "",
-    "nonvoluntary_ctxt_switches" => "",
-  }
 
-  macro define_getters(*keys)
-    {% for key in keys %}
-      def {{ key.downcase.id }}
-        {{ @data[key] }}
-      end
-    {% end %}
-  end
+  KEYS = [
+    "Name",
+    "Umask",
+    "State",
+    "Tgid",
+    "Ngid",
+    "Pid",
+    "PPid",
+    "TracerPid",
+    "Uid",
+    "Gid",
+    "FDSize",
+    "Groups",
+    "NStgid",
+    "NSpid",
+    "NSpgid",
+    "NSsid",
+    "VmPeak",
+    "VmSize",
+    "VmLck",
+    "VmPin",
+    "VmHWM",
+    "VmRSS",
+    "RssAnon",
+    "RssFile",
+    "RssShmem",
+    "VmData",
+    "VmStk",
+    "VmExe",
+    "VmLib",
+    "VmPTE",
+    "VmSwap",
+    "HugetlbPages",
+    "CoreDumping",
+    "THP_enabled",
+    "Threads",
+    "SigQ",
+    "SigPnd",
+    "ShdPnd",
+    "SigBlk",
+    "SigIgn",
+    "SigCgt",
+    "CapInh",
+    "CapPrm",
+    "CapEff",
+    "CapBnd",
+    "CapAmb",
+    "NoNewPrivs",
+    "Seccomp",
+    "Seccomp_filters",
+    "Speculation_Store_Bypass",
+    "SpeculationIndirectBranch",
+    "Cpus_allowed",
+    "Cpus_allowed_list",
+    "Mems_allowed",
+    "Mems_allowed_list",
+    "voluntary_ctxt_switches",
+    "nonvoluntary_ctxt_switches"
+  ]	
+
+  getter data : Hash(String, String)
+
+  {% for key in KEYS %}
+    def {{ key.downcase.id }}
+      data[{{key}}]
+    end
+  {% end %}
   
-  define_getters @data.keys
- 
   enum Field
     Key
     Blank
@@ -76,6 +75,8 @@ struct Hardware::PID::Status
   end
 
   protected def initialize(io : IO)
+    @data = Hash(String, String).new
+    KEYS.each { |k| @data[k] = "" }
     buffer = IO::Memory.new
     key = ""
     state = Field::Key
