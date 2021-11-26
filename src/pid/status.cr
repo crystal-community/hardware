98eb17ea7dab4f76e55b642997b3554aa0da0402
Gabriel Aires Guedes <airesgabriel@gmail.com>
2021-11-26 10:25:05 -0300

    auto generate getters for Hardware::Pid::Status
    new names for state enum

--- src/pid/status.cr
+++ src/pid/status.cr
@@ -59,65 +59,17 @@ struct Hardware::PID::Status
     "nonvoluntary_ctxt_switches" => "",
   }
 
-  getter name : String { @data["Name"] }
-  getter umask : String { @data["Umask"] }
-  getter state : String { @data["State"] }
-  getter tgid : String { @data["Tgid"] }
-  getter ngid : String { @data["Ngid"] }
-  getter pid : String { @data["Pid"] }
-  getter ppid : String { @data["PPid"] }
-  getter tracerpid : String { @data["TracerPid"] }
-  getter uid : String { @data["Uid"] }
-  getter gid : String { @data["Gid"] }
-  getter fdsize : String { @data["FDSize"] }
-  getter groups : String { @data["Groups"] }
-  getter nstgid : String { @data["NStgid"] }
-  getter nspid : String { @data["NSpid"] }
-  getter nspgid : String { @data["NSpgid"] }
-  getter nssid : String { @data["NSsid"] }
-  getter vmpeak : String { @data["VmPeak"] }
-  getter vmsize : String { @data["VmSize"] }
-  getter vmlck : String { @data["VmLck"] }
-  getter vmpin : String { @data["VmPin"] }
-  getter vmhwm : String { @data["VmHWM"] }
-  getter vmrss : String { @data["VmRSS"] }
-  getter rssanon : String { @data["RssAnon"] }
-  getter rssfile : String { @data["RssFile"] }
-  getter rssshmem : String { @data["RssShmem"] }
-  getter vmdata : String { @data["VmData"] }
-  getter vmstk : String { @data["VmStk"] }
-  getter vmexe : String { @data["VmExe"] }
-  getter vmlib : String { @data["VmLib"] }
-  getter vmpte : String { @data["VmPTE"] }
-  getter vmswap : String { @data["VmSwap"] }
-  getter hugetlbpages : String { @data["HugetlbPages"] }
-  getter coredumping : String { @data["CoreDumping"] }
-  getter thp_enabled : String { @data["THP_enabled"] }
-  getter threads : String { @data["Threads"] }
-  getter sigq : String { @data["SigQ"] }
-  getter sigpnd : String { @data["SigPnd"] }
-  getter shdpnd : String { @data["ShdPnd"] }
-  getter sigblk : String { @data["SigBlk"] }
-  getter sigign : String { @data["SigIgn"] }
-  getter sigcgt : String { @data["SigCgt"] }
-  getter capinh : String { @data["CapInh"] }
-  getter capprm : String { @data["CapPrm"] }
-  getter capeff : String { @data["CapEff"] }
-  getter capbnd : String { @data["CapBnd"] }
-  getter capamb : String { @data["CapAmb"] }
-  getter nonewprivs : String { @data["NoNewPrivs"] }
-  getter seccomp : String { @data["Seccomp"] }
-  getter seccomp_filters : String { @data["Seccomp_filters"] }
-  getter speculation_store_bypass : String { @data["Speculation_Store_Bypass"] }
-  getter speculationindirectbranch : String { @data["SpeculationIndirectBranch"] }
-  getter cpus_allowed : String { @data["Cpus_allowed"] }
-  getter cpus_allowed_list : String { @data["Cpus_allowed_list"] }
-  getter mems_allowed : String { @data["Mems_allowed"] }
-  getter mems_allowed_list : String { @data["Mems_allowed_list"] }
-  getter voluntary_ctxt_switches : String { @data["voluntary_ctxt_switches"] }
-  getter nonvoluntary_ctxt_switches : String { @data["nonvoluntary_ctxt_switches"] }
-
-  enum ReadState
+  macro define_getters(*keys)
+    {% for key in keys %}
+      def {{ key.downcase.id }}
+        {{ @data[key] }}
+      end
+    {% end %}
+  end
+  
+  define_getters @data.keys
+ 
+  enum Field
     Key
     Blank
     Value
@@ -126,33 +78,37 @@ struct Hardware::PID::Status
   protected def initialize(io : IO)
     buffer = IO::Memory.new
     key = ""
-    state = ReadState::Key
+    state = Field::Key
 
     io.each_char do |char|
+      
       case state
-      when ReadState::Key
+      when Field::Key
         if char == ':'
           key = buffer.to_s
           buffer.clear
-          state = ReadState::Blank
+          state = Field::Blank
         else
           buffer << char
         end
-      when ReadState::Blank
+      
+      when Field::Blank
         if char.alphanumeric?
           buffer << char
-          state = ReadState::Value
+          state = Field::Value
         end
-      when ReadState::Value
+    
+      when Field::Value
         if char == '\n'
           data[key] = buffer.to_s
           buffer.clear
-          state = ReadState::Key
+          state = Field::Key
           key = ""
         else
           buffer << char
         end
       end
+
     end
   end
 end
